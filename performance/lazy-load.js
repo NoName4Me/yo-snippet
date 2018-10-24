3f4667a401f1f46d43b60ca51b65616156714ca0
dengzhongzhe <dengzhongzhe@mafengwo.com>
2018-10-24 16:08:40 +0800

    懒加载，增加视频部分。

--- performance/lazy-load.js
+++ performance/lazy-load.js
@@ -1,67 +1,88 @@
 export default function () {
-  let lazyImages = []
+  let lazyResources = []
   let active = false
   return {
     observe (selector = '.lazy', scrollBox) {
-      lazyImages = [].slice.call(document.querySelectorAll(selector))
+      lazyResources = [].slice.call(document.querySelectorAll(selector))
 
-      if ("IntersectionObserver" in window) {
-        let lazyImageObserver = new IntersectionObserver((entries, observer) => {
+      if ('IntersectionObserver' in window) {
+        let lazyResourceObserver = new IntersectionObserver((entries, observer) => {
           entries.forEach((entry) => {
             if (entry.isIntersecting) {
-              let lazyImage = entry.target;
-              updateImage(lazyImage)
-              lazyImageObserver.unobserve(lazyImage);
+              let lazyResource = entry.target
+              doUpdate(lazyResource)
+              lazyResourceObserver.unobserve(lazyResource)
             }
-          });
-        });
+          })
+        })
 
-        lazyImages.forEach((lazyImage) => {
-          lazyImageObserver.observe(lazyImage);
-        });
+        lazyResources.forEach((lazyResource) => {
+          lazyResourceObserver.observe(lazyResource)
+        })
       } else {
         if (scrollBox) {
           let scrollTarget = scrollBox || document
-          scrollTarget.addEventListener("scroll", lazyLoadTask);
+          scrollTarget.addEventListener('scroll', lazyLoadTask)
         }
 
-        window.addEventListener("resize", lazyLoadTask);
-        window.addEventListener("orientationchange", lazyLoadTask);
-        lazyLoadTask();
+        window.addEventListener('resize', lazyLoadTask)
+        window.addEventListener('orientationchange', lazyLoadTask)
+        lazyLoadTask()
       }
     },
   }
 
   function updateImage (lazyImage) {
-    lazyImage.src = lazyImage.dataset.src;
-    lazyImage.srcset = lazyImage.dataset.srcset || '';
-    lazyImage.classList.remove("lazy");
+    lazyImage.src = lazyImage.dataset.src
+    lazyImage.srcset = lazyImage.dataset.srcset || ''
+    lazyImage.classList.remove('lazy')
+  }
+
+  function updateVideo (lazyVideo) {
+    for (let source in lazyVideo.children) {
+      let videoSource = lazyVideo.children[source]
+      if (typeof videoSource.tagName === 'string' && videoSource.tagName === 'SOURCE') {
+        videoSource.src = videoSource.dataset.src
+      }
+    }
+    lazyVideo.load()
+    lazyVideo.classList.remove('lazy')
+  }
+
+  function doUpdate (target) {
+    if (target) {
+      if (target.tagName === 'IMG') {
+        updateImage(target)
+      } else if (target.tagName === 'VIDEO') {
+        updateVideo(target)
+      }
+    }
   }
 
   function lazyLoadTask () {
     if (active === false) {
       active = true
       setTimeout(() => {
-        lazyImages.forEach((lazyImage) => {
-          if ((lazyImage.getBoundingClientRect().top <= window.innerHeight && lazyImage.getBoundingClientRect().bottom >= 0) && getComputedStyle(lazyImage).display !== "none") {
-            lazyImage.src = lazyImage.dataset.src;
-            lazyImage.srcset = lazyImage.dataset.srcset || '';
-            lazyImage.classList.remove("lazy");
+        lazyResources.forEach((lazyResource) => {
+          if ((lazyResource.getBoundingClientRect().top <= window.innerHeight
+            && lazyResource.getBoundingClientRect().bottom >= 0)
+            && getComputedStyle(lazyResource).display !== 'none') {
+            doUpdate(lazyResource)
 
-            lazyImages = lazyImages.filter((image) => {
-              return image !== lazyImage;
-            });
+            lazyResources = lazyResources.filter((image) => {
+              return image !== lazyResource
+            })
 
-            if (lazyImages.length === 0) {
-              document.removeEventListener("scroll", lazyLoadTask);
-              window.removeEventListener("resize", lazyLoadTask);
-              window.removeEventListener("orientationchange", lazyLoadTask);
+            if (lazyResources.length === 0) {
+              document.removeEventListener('scroll', lazyLoadTask)
+              window.removeEventListener('resize', lazyLoadTask)
+              window.removeEventListener('orientationchange', lazyLoadTask)
             }
           }
-        });
+        })
 
-        active = false;
-      }, 200);
+        active = false
+      }, 200)
     }
   }
 }
--- performance/readme.md
+++ performance/readme.md
@@ -2,13 +2,20 @@
 
 ## 懒加载（图片/视频）
 
-```html
-<img class="lazy" src="default.png" data-src="true-image.png" data-srcset="true-image-2x.png 2x, true-image.png 1x">
-```
-
 ```js
 import lazyLoad from './lazy-load.js'
 
 lazyLoad().observe()
+// observe(selector, scrollBox)
+//   @selector: 默认 '.lazy'
+//   @scrollBox: 懒加载监听的滚动盒，默认 document
+```
 
+```html
+<img class="lazy" src="default.png" data-src="true-image.png" data-srcset="true-image-2x.png 2x, true-image.png 1x">
+
+<video class="lazy" autoplay loop playsinline width="375" height="127" poster="the-poster.jpg">
+  <source data-src="true-video.webm">
+  <source data-src="true-video.mp4">
+</video>
 ```
